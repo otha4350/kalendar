299e6b9c4da2b1227f84cb95e0c6310d0a9ab389
Otto Hammar <otto.hammar.4350@student.uu.se>
2025-08-10 18:51:04 +0200

    multiday events funkar, progress på palette grejen

--- draw_cal.py
+++ draw_cal.py
@@ -1,11 +1,11 @@
-from icalevents import icalevents
+from icalevents import icalevents, icalparser
 import csv
 from PIL import Image, ImageDraw, ImageFont, ImageColor, features
 import calendar
 import datetime
 import locale
 import holidays
-
+import numpy
 
 
 # Constants for calendar layout
@@ -24,7 +24,7 @@ c_white = "white"
 c_yellow = "yellow"
 c_red = "red"
 c_blue = "blue"
-c_green = "green"
+c_green = "rgb(0, 255, 0)"
 
 
 
@@ -56,25 +56,50 @@ class DrawCalendarDay:
             d.text((x2 - 5, y1 + 3), week_str, font=FONT, fill=weeknum_color, anchor="rt")
 
         # Draw events
-        todays_events = [e for e in events if e[0].start.date() == self.date]
-        todays_events.sort(key=lambda x: x[0].start)
+        todays_events = []
+        multiday_event_days = {}
+
+        # Grab multiday events
+        for event, color in events:
+            if event.start.date() <= self.date <= event.end.date() and not event.start.date() == event.end.date() and not event.all_day:
+                event_len = (event.end.date() - event.start.date()).days + 1
+                event_day = (self.date - event.start.date()).days + 1
+                event_day = f"({event_day}/{event_len})"
+                multiday_event_days[event] = event_day
+                todays_events.append((event, color))
+            
+        todays_oneday_events = [e for e in events if e[0].start.date() == self.date and not e[0] in multiday_event_days.keys()]
+        todays_oneday_events.sort(key=lambda x: x[0].start)
+        todays_events.extend(todays_oneday_events)
 
         events_today = len(todays_events)
         if events_today > MAX_EVENTS:
             todays_events = todays_events[:MAX_EVENTS-1]
         for idx, (event, color) in enumerate(todays_events):
 
+            # draw bullet point
             bp = "★" if color == "5" else "❤" if color == "3" else "*"
             d.text((x1+2, y1 +3+ (12 * (idx+1)) - 2), bp, font=SYMBOL_FONT, fill=int(color))
             bp_w = d.textlength(bp, font=SYMBOL_FONT)
 
-
-            event_text = f"{event.start.strftime('%H')} {event.summary}" if not event.all_day else event.summary
-            unacceptable_textlen = lambda text: d.textlength(text + "...", font=FONT) > self.w - (bp_w)
-            if unacceptable_textlen(event_text):
-                while unacceptable_textlen(event_text):
-                    event_text = event_text[:-1]
-                event_text += "..."
+            event_text = ""
+            if event in multiday_event_days.keys():
+                event_text = event.summary
+                days_string = multiday_event_days[event]
+                unacceptable_textlen = lambda text: d.textlength(text + "..." + days_string, font=FONT) > self.w - (bp_w)
+                if unacceptable_textlen(event_text):
+                    while unacceptable_textlen(event_text):
+                        event_text = event_text[:-1]
+                    event_text += "..." + days_string
+                else:
+                    event_text += " " + days_string
+            else:
+                event_text = f"{event.start.strftime('%H')} {event.summary}" if not event.all_day else event.summary
+                unacceptable_textlen = lambda text: d.textlength(text + "...", font=FONT) > self.w - (bp_w)
+                if unacceptable_textlen(event_text):
+                    while unacceptable_textlen(event_text):
+                        event_text = event_text[:-1]
+                    event_text += "..."
             d.text((x1 + 2+bp_w, y1 +3+ (12 * (idx+1))), event_text, fill=int(color), font=FONT)
         
         if  events_today > MAX_EVENTS:
@@ -130,12 +155,12 @@ class DrawCalendar:
 
 
 def setup_image():
-    out = Image.new("P", (IMG_WIDTH, IMG_HEIGHT), background_color)
+    out = Image.new("RGB", (IMG_WIDTH, IMG_HEIGHT), background_color)
     d = ImageDraw.Draw(out)
     d.rectangle([0, 0, IMG_WIDTH, IMG_HEIGHT], fill=background_color)
     return out, d
 
-def do_stuff():
+def  do_stuff():
     global background_color, weekday_color, weeknum_color, month_color, lines_color, today_box_color, red_day_color
     background_color = c_white
     weekday_color = c_black
@@ -149,7 +174,7 @@ def do_stuff():
     out, d = setup_image()
     cal = DrawCalendar(CAL_X, CAL_Y, CAL_W, CAL_H)
 
-    events = []
+    events: list[tuple[icalevents.Event, str]] = []
     with open("calendars.csv", "r") as csvfile:
         reader = csv.reader(csvfile)
         reader.__next__()
@@ -167,5 +192,62 @@ if __name__ == "__main__":
     
     out = do_stuff()
     print(out.getcolors())
-    out.show()
+
+    DESATURATED_PALETTE = [
+        [0, 0, 0],
+        [255, 255, 255],
+        [255, 255, 0],
+        [255, 0, 0],
+        [0, 0, 255],
+        [0, 255, 0],
+        [255, 255, 255]]
+
+    SATURATED_PALETTE = [
+        [0, 0, 0],
+        [161, 164, 165],
+        [208, 190, 71],
+        [156, 72, 75],
+        [61, 59, 94],
+        [58, 91, 70],
+        [255, 255, 255]]
+   
+    def _palette_blend(saturation, dtype="uint8"):
+        saturation = float(saturation)
+        palette = []
+        for i in range(6):
+            rs, gs, bs = [c * saturation for c in SATURATED_PALETTE[i]]
+            rd, gd, bd = [c * (1.0 - saturation) for c in DESATURATED_PALETTE[i]]
+            if dtype == "uint8":
+                palette += [int(rs + rd), int(gs + gd), int(bs + bd)]
+            if dtype == "uint24":
+                palette += [(int(rs + rd) << 16) | (int(gs + gd) << 8) | int(bs + bd)]
+        return palette
+    
+    dither = Image.Dither.FLOYDSTEINBERG
+
+    # Image size doesn't matter since it's just the palette we're using
+    palette_image = Image.new("P", (1, 1))
+
+    if out.mode == "P":
+        print("P")
+        # Create a pure colour palette from DESATURATED_PALETTE
+        palette = numpy.array(DESATURATED_PALETTE, dtype=numpy.uint8).flatten().tobytes()
+        palette_image.putpalette(palette)
+
+        # Assume that palette mode images with an unset palette use the
+        # default colour order and "DESATURATED_PALETTE" pure colours
+        if not out.palette.colors:
+            out.putpalette(palette)
+
+        # Assume that palette mode images with exactly six colours use
+        # all the correct colours, but not exactly in the right order.
+        if len(out.palette.colors) == 6:
+            dither = Image.Dither.NONE
+    else:
+        # All other image should be quantized and dithered
+        palette = _palette_blend(0)
+        palette_image.putpalette(palette)
+
+    image = out.convert("RGB").quantize(6, palette=palette_image, dither=dither)
+    image.show()
 
