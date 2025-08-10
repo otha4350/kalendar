60554b740e5b453c92c4c869109df1873a73c2e1
Otto Hammar <otto.hammar.4350@student.uu.se>
2025-08-10 22:43:56 +0200

    removed custom inky colors

--- show_on_inky.py
+++ show_on_inky.py
@@ -5,13 +5,6 @@ import draw_cal
 if __name__ == "__main__":
     inky = auto(ask_user=True, verbose=True)
 
-    draw_cal.c_black = 0
-    draw_cal.c_white = 1
-    draw_cal.c_yellow = 2
-    draw_cal.c_red = 3
-    draw_cal.c_blue = 4
-    draw_cal.c_green = 5
-
     out = draw_cal.do_stuff()
     inky.set_image(out)
     inky.show()
