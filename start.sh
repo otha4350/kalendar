7afb1b2626bb1bc09ea9c007b3c9930f18126694
Otto Hammar <otto.hammar.4350@student.uu.se>
2025-08-10 17:43:09 +0200

    duvet när man städar innan en kompis kommer förbi

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -x
+
+git reset --hard origin/master
+git clean -df
+git pull
+
 source ~/.virtualenvs/pimoroni/bin/activate
 python ~/kalendar/show_on_inky.py
--- take_screenshot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cargo run &
-sleep 10
-
-chromium-browser --headless --disable-gpu --screenshot=screenshot.png -window-size=800,480 "http://localhost:3001"
-
-source ~/.virtualenvs/pimoroni/bin/activate
-
-./image.py --file screenshot.png
